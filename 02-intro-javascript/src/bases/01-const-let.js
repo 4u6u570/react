8f22b0c99bd32038222be9bcb12944cd43fa982c
Augusto <augustosalazar@outlook.com>
2020-08-19 01:03:34 -0300

    Bases repaso JS

---
+++ 02-intro-javascript/src/bases/01-const-let.js
@@ -0,0 +1,17 @@
+// Variables y Constantes
+
+const nombre = 'Augusto';
+const apellido = 'Salazar';
+
+let valorDado = 5;
+valorDado = 4;
+
+console.log( nombre, apellido );
+
+// VAR ya no se debe usar.
+if ( true ) {
+    const nombre = 'Peter';
+    console.log(nombre);
+}
+
+console.log( valorDado );
