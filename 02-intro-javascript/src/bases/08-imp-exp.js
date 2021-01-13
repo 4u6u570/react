b976765885b399c7adcdb1083600361d01028815
Augusto <augustosalazar@outlook.com>
2021-01-12 23:44:49 -0300

    Updates

--- 02-intro-javascript/src/bases/08-imp-exp.js
+++ 02-intro-javascript/src/bases/08-imp-exp.js
@@ -1,24 +1,20 @@
-// import { heroes } from '../data/heroes';
-import heroes, { owners } from '../data/heroes';
-// import
+// import { heroes } from './data/heroes';
+// import { heroes } from './data/heroes';
+// import heroes, { owners } from '../data/heroes';
+import heroes from '../data/heroes';
 
-console.log( heroes );
+// console.log( owners );
 
-// const getHeroeById = (id) => {
-//     return heroes.find( ( heroe ) => {
-//         if heroe.id === id ) {
-//             return true;
-//         } else {
-//             return false;
-//         }
-//     });
-// }
 
-const getHeroeById = (id) => heroes.find( ( heroe ) => heroe.id === id );
 
-console.log( getHeroeById(2) );
+
+export const getHeroeById = (id) => heroes.find( (heroe) => heroe.id === id );
+
+// console.log( getHeroeById(2) );
 
 // find?, filter
-const getHeroeByOwner = ( owner ) => heroes.filter( ( heroe ) => heroe.owner === owner );
+export const getHeroesByOwner = ( owner ) => heroes.filter( (heroe) => heroe.owner === owner );
 
-console.log( getHeroeByOwner('DC') );
+// console.log( getHeroesByOwner('Marvel') );
+
+
